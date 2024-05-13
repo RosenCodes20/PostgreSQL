6b1c6c749375c5c756bb208c8093577ea100b370
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-05-14 00:20:23 +0300

    Add files via upload

---
+++ postgresql_data_types_and_variables/create_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE employees(
+	id serial PRIMARY KEY NOT NULL,
+	first_name VARCHAR(30),
+	last_name VARCHAR(50),
+	hiring_date date,
+	salary numeric(10, 2),
+	devices_number integer
+);
+
+CREATE TABLE departments(
+	id serial PRIMARY KEY NOT NULL,
+	name VARCHAR(50),
+	code CHAR(3),
+	description TEXT
+);
+
+CREATE TABLE issues(
+	id serial PRIMARY KEY UNIQUE,
+	description VARCHAR(150),
+	date date,
+	start timestamp without time zone
+);
